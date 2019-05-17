f2a823162d0bbed5ee116768010c607cf80a1c27
Игорь Карпук <igorek.karpuk@gmail.com>
2019-05-17 11:37:42 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import Root from './components/Root';
 
-const token = '5238b02305a72c63561bfb3bddd416242fc37ad1';
+const token = '5a55f030baec427060667a12edc420290f195653';
 
 const httpLink = {
   uri: 'https://api.github.com/graphql',
